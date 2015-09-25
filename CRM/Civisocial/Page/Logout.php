27227aa82e2502ab027a96ba77d4b02fbe22a96e
Siddharth Gupta <siddharth11110@iiitd.ac.in>
2015-09-25 22:26:03 +0530

    added logout

---
+++ CRM/Civisocial/Page/Logout.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_Civisocial_Page_Logout extends CRM_Core_Page {
+	function run() {
+		$session = CRM_Core_Session::singleton();
+		$session->set('userID', NULL);
+		$session->set('backend', NULL);
+		return CRM_Utils_System::redirect(CRM_Utils_System::url('', NULL, TRUE));
+	}
+}
