2c5567bec1de39454b10db0e43d22c6ee8629cb5
Dilip Raj Baral <rajb.dilip@gmail.com>
2016-06-04 21:24:40 +0530

    Remove no longer needed  function

--- CRM/Civisocial/Page/OAuthCallback.php
+++ CRM/Civisocial/Page/OAuthCallback.php
@@ -7,30 +7,7 @@ require_once 'CRM/Civisocial/Backend/OAuthProvider/Twitter.php';
 
 class CRM_Civisocial_Page_OAuthCallback extends CRM_Core_Page {
 
-	function get_response($apiURL, $node, $is_post, $params) {
-		$url = $apiURL . "/" . $node;
-		$urlparams = "";
-		foreach ($params as $key => $value) {
-			$urlparams .= $key . "=" . $value . "&";
-		}
-		if ($is_post == FALSE) {
-			$url = $url . "?" . $urlparams;
-		}
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		if ($is_post == TRUE) {
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $urlparams);
-		} else {
-			curl_setopt($ch, CURLOPT_POST, 0);
-		}
-		$response = curl_exec($ch);
-		curl_close($ch);
-
-		return json_decode($response, true);
-	}
-
-	function run() {
+	public function run() {
 		$path = CRM_Utils_System::currentPath();
 		if (false !== strpos($path, '..')) {
 			exit("Fatal Error: the URL can't contain '..'. Please report the issue on the forum at civicrm.org");
