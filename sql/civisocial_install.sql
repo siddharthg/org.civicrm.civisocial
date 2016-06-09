2f0ec4237415416d420a512e3353022e0073c59a
Dilip Raj Baral <rajb.dilip@gmail.com>
2016-06-10 02:42:50 +0530

    Refactor OAuthProvider class  - Added `saveSocialuser()` method to `OAuthProvider` class to remove redundant    lines of code in child classes `handleCallback()` methods  - `getUserProfile()` of all child classes now returns array with same keys  - Now serialized access token string is store in the table under field    `access_token`. Hence, Twitter's both oauth_token and oauth_secret is stored    in a single fields. It hence eliminated the need of `oauth_secret` field

--- sql/civisocial_install.sql
+++ sql/civisocial_install.sql
@@ -9,15 +9,14 @@ DROP TABLE IF EXISTS `civicrm_civisocial_user`;
 --
 
 CREATE TABLE `civicrm_civisocial_user` (
-     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique User ID',
-     `contact_id` int unsigned NULL DEFAULT NULL COMMENT 'FK to Contact ID that owns that account',
-     `oauth_provider` varchar(128) NOT NULL COMMENT 'OAuth Provider',
-     `social_user_id` varchar(128) NULL DEFAULT NULL COMMENT 'User identifier given by OAuth Provider',
-     `oauth_token` varchar(512) NULL DEFAULT NULL COMMENT 'Access Token Provided by OAuth Provider',
-     `oauth_secret` varchar(1024) NULL DEFAULT NULL COMMENT 'Access Token Secret Provided by OAuth Provider',
-     `created_date` timestamp NULL DEFAULT NULL COMMENT 'When was the civisocial user was created.',
-     `modified_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was the the civisocial user was created or modified or deleted.'
-, PRIMARY KEY ( `id` ),
-, UNIQUE KEY ( `contact_id`, `oauth_provider`, `social_user_id`)
-, CONSTRAINT FK_civicrm_civisocial_user_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL
+  `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique User ID',
+  `contact_id` int unsigned NULL DEFAULT NULL COMMENT 'FK to Contact ID that owns that account',
+  `oauth_provider` varchar(128) NOT NULL COMMENT 'OAuth Provider',
+  `social_user_id` varchar(128) NULL DEFAULT NULL COMMENT 'User identifier given by OAuth Provider',
+  `access_token` varchar(1024) NULL DEFAULT NULL COMMENT 'Access Token provided by OAuth Provider',
+  `created_date` timestamp NULL DEFAULT NULL COMMENT 'When was the civisocial user was created.',
+  `modified_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was the the civisocial user was created or modified or deleted.',
+	PRIMARY KEY ( `id` ),
+	UNIQUE KEY ( `contact_id`, `oauth_provider`, `social_user_id`),
+	CONSTRAINT FK_civicrm_civisocial_user_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
