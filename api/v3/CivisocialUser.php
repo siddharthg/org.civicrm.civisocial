f2297631ec22ece87e3e0375840e5d00c14d2c41
Dilip Raj Baral <rajb.dilip@gmail.com>
2016-07-19 02:51:26 +0800

    Autofill Add New Event form using Facebook event info

--- api/v3/CivisocialUser.php
+++ api/v3/CivisocialUser.php
@@ -3,7 +3,7 @@
  * Create a social user.
  *
  * @param array $params
- * 
+ *
  * @return int|bool
  *   Returns ID if exits, FALSE otherwise
  */
@@ -16,7 +16,7 @@ function civicrm_api3_civisocial_user_socialUserExists($params) {
  * Create a social user.
  *
  * @param array $params
- * 
+ *
  * @return array
  *   Array of created values
  */
@@ -29,12 +29,40 @@ function civicrm_api3_civisocial_user_create($params) {
  * Creates a contact if doesn't exist and returns it's id.
  *
  * @param array $params
- * 
+ *
  * @return int
  *   Contact id of created/existing contact
  */
 function civicrm_api3_civisocial_user_createContact($params) {
-	// @todo: Add `email` after fixing Twitter's email retrieval
+  // @todo: Add `email` after fixing Twitter's email retrieval
   civicrm_api3_verify_mandatory($params, NULL, array('contact_type'));
   return CRM_Civisocial_BAO_CivisocialUser::createContact($params);
+}
+
+function civicrm_api3_civisocial_user_getFacebookEventInfo($params) {
+  $session = CRM_Core_Session::singleton();
+  $fbAccessToken = $session->get('facebook_access_token');
+  if ($fbAccessToken) {
+    $facebook = new CRM_Civisocial_OAuthProvider_Facebook($fbAccessToken);
+    if ($facebook->isAuthorized()) {
+      $eventId = $params['event_id'];
+      $eventInfo = $facebook->get($eventId, array('fields' => 'name,description,place,start_time,end_time'));
+      if ($eventInfo) {
+        $startTime = strtotime($eventInfo['start_time']);
+        $endTime = strtotime($eventInfo['end_time']);
+        unset($eventInfo['start_time']);
+        unset($eventInfo['end_time']);
+
+        $eventInfo['start_date'] = date('m/d/Y', $startTime);
+        $eventInfo['start_time'] = date('h:iA', $startTime);
+        $eventInfo['end_date'] = date('m/d/Y', $endTime);
+        $eventInfo['end_time'] = date('h:iA', $endTime);
+        return $eventInfo;
+      }
+      else {
+        return civicrm_api3_create_error("The facebook event either doesn't exist or is private.");
+      }
+    }
+  }
+  return civicrm_api3_create_error("Not connected to Facebook.");
 }
--- templates/res/js/facebook-event.js
+++ templates/res/js/facebook-event.js
@@ -1,3 +1,36 @@
 CRM.$(function($) {
+  var waitingForResponse = false;
+  cj('#fetch_fb_event_info').click(function() {
+    if (waitingForResponse) {
+      return;
+    }
+    cj(this).html('Fetching..');
+    waitingForResponse = true;
 
+    var match = cj('#facebook_event_url').val().match(/facebook\.com\/events\/([0-9]+)\/?/m);
+    if (match) {
+      eventId = match[1];
+    }
+    console.log(match);
+
+    CRM.api3('CivisocialUser', 'getfacebookeventinfo', {
+      "event_id": eventId
+    }).done(function(result) {
+      console.log(result);
+      if (result.is_error) {
+        // Display error
+      }
+      else {
+        cj('#title').val(result.name);
+        CKEDITOR.instances.description.setData(result.description);
+        cj('[id^=start_date_display]').val(result.start_date);
+        cj('#start_date_time').val(result.start_time);
+        cj('[id^=end_date_display]').val(result.end_date);
+        cj('#end_date_time').val(result.end_time);
+      }
+
+      cj('#fetch_fb_event_info').html('Fetch Info');
+      waitingForResponse = false;
+    });
+  });
 });
